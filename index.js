b983f93d73d1a0c75ed494f8f9eb31dd6f1034df
dump501 <tsafack07@gmail.com>
2023-08-29 18:21:08 +0100

    init the lib

---
+++ index.js
@@ -0,0 +1,7 @@
+if(typeof exports != "undefined"){    
+    exports.$ = document.querySelector.bind(document);    
+    exports.$$ = document.querySelectorAll.bind(document);
+  }else{    
+    var $ = document.querySelector.bind(document);    
+    var $$ = document.querySelectorAll.bind(document);
+  }
